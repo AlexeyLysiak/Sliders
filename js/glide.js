39c3e1adeaafed0526a0c2528d4baffc57332610
Alexey Lysiak <lysiak.oleksii@gmail.com>
2020-05-06 21:42:38 +0300

    create sliders

---
+++ js/glide.js
@@ -0,0 +1,7 @@
+const config = {
+  type: 'carousel',
+  perView: 3,
+  keyboard: true,
+  gap: 15,
+}
+new Glide('.glide', config).mount()
---
+++ js/slick.js
@@ -0,0 +1,34 @@
+$('.slider').slick({
+  dots: true,
+  infinite: false,
+  speed: 1000,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+        
